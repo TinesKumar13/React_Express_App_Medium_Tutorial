d2678f088fb239eabbf7a7d24f8ca96e65f69878
Dev Nesh <tineshreds@gmail.com>
2024-04-20 17:16:28 +0800

    - Added right url for be api

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   callAPI() {
-    fetch("http://localhost:80/testAPI")
+    fetch("http://10.2.0.4:80/testAPI")
       .then((res) => res.text())
       .then((res) => this.setState({ apiResponse: res }))
       .catch((err) => err);
